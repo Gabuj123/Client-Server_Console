0cc3f6e2af8fb841046e45a9d79774700bdc31bc
Gabriele <gabriele.borelli@hotmail.com>
2024-10-25 11:29:55 +0200

    Aggiunti i file

---
+++ ClientSocket/ClientSocket/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+public class SynchronousSocketClient
+{
+    public static void StartClient()
+    {
+        // Data buffer for incoming data.
+        byte[] bytes = new byte[1024];
+        int scelta;
+        string messaggio;
+        string nick;
+        string ip;
+        //legge nickname
+        Console.WriteLine();
+        Console.WriteLine("Mini Chat");
+        Console.Write("Nickname:");
+        nick = Convert.ToString(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("IP del server:");
+        ip = Convert.ToString(Console.ReadLine());
+        scelta = 0;
+        // Connect to a remote device.
+        try
+        {
+            while (scelta != 3)
+            {
+                // Establish the remote endpoint for the socket.
+                // This example uses port 11000 on the local computer.
+                IPAddress ipAddress = IPAddress.Parse(ip);
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+                // Create a TCP/IP  socket.
+                Socket sender = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+
+
+                //legge scelta
+                Console.WriteLine();
+                Console.WriteLine("1. Leggi messaggio");
+                Console.WriteLine("2. Scrivi messaggio");
+                Console.WriteLine("3. Esci");
+                scelta = Convert.ToInt32(Console.ReadLine());
+                messaggio = "<EOF>";
+                if (scelta == 1)
+                {
+                    messaggio = "<RDMSG><EOF>";
+                }
+                if (scelta == 2)
+                {
+                    Console.WriteLine("Scrivi messaggio:");
+                    messaggio = nick + ":" + Convert.ToString(Console.ReadLine()) + "<EOF>";
+                }
+                if (scelta == 3)
+                {
+                    Console.WriteLine("Fine");
+                    Environment.Exit(0);
+                }
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    sender.Connect(remoteEP);
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.UTF8.GetBytes(messaggio);
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.UTF8.GetString(bytes, 0, bytesRec));
+                    // Release the socket.
+                    // sender.Shutdown(SocketShutdown.Both);
+                    // sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+    public static int Main(String[] args)
+    {
+        StartClient();
+        Console.ReadLine();
+        return 0;
+    }
+}
---
+++ ServerSocket/ServerSocket/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+public class SynchronousSocketListener
+{
+    // Incoming data from the client.
+    public static string data = null;
+    // memorizza ultimo messaggio
+    public static string messaggio = "<EOF>";
+    // prepara testo per la risposta
+    public static string risposta = "<EOF>";
+
+    public static void StartListening()
+    {
+        // Data buffer for incoming data.
+        byte[] bytes = new Byte[2048];
+        // Establish the local endpoint for the socket.
+        // Dns.GetHostName returns the name of the 
+        // host running the application.
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress[] addr = ipHostInfo.AddressList;
+        for (int i = 0; i < addr.Length; i++)
+        {
+            Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+        }
+        IPAddress ipAddress = ipAddress = addr[addr.Length - 2];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        Console.WriteLine("Server IP {0} ", ipAddress.ToString());
+        // Create a TCP/IP socket.
+        Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+        listener.ReceiveTimeout = 500;
+        // Bind the socket to the local endpoint and 
+        // listen for incoming connections.
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            // Start listening for connections.
+            while (true)
+            {
+                Console.WriteLine("In ascolto su {0} ...", localEndPoint.ToString());
+                // Program is suspended while waiting for an incoming connection.
+                Socket handler = listener.Accept();
+                data = null;
+                // An incoming connection needs to be processed.
+                while (true)
+                {
+                    bytes = new byte[2048];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+                // interpreto il comando
+                if (data == "<RDMSG><EOF>")
+                {
+                    risposta = messaggio;
+                }
+                else
+                {
+                    messaggio = data;
+                    risposta = "<ACK><EOF>";
+                    // Show the data on the console.
+                    Console.WriteLine("{0} - {1}", handler.RemoteEndPoint.ToString(), messaggio);
+                    //Console.WriteLine(messaggio);
+                }
+
+
+                // Echo the data back to the client.
+                byte[] msg = Encoding.UTF8.GetBytes(risposta);
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        //Console.WriteLine("\nPress ENTER to continue...");
+        //Console.Read();
+
+
+        //var fileName = GetExecutingAssembly().Location;
+        //System.Diagnostics.Process.Start(fileName);
+        // Closes the current process
+        //Environment.Exit(0);
+        string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        System.Diagnostics.Process.Start(strExeFilePath);
+    }
+    public static int Main(String[] args)
+    {
+        StartListening();
+        return 0;
+    }
+}
